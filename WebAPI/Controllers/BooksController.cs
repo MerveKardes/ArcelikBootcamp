735a44645637b0577a7729662f9113c13a11bf3a
sevgiaykir <sevgiaykirr@gmail.com>
2022-05-05 16:27:09 +0300

    Created controller for Book

--- WebAPI/Controllers/BooksController.cs
+++ WebAPI/Controllers/BooksController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI.Application.CQRS.Commands;
+using WebAPI.Application.CQRS.Queries;
 
 namespace WebAPI.Controllers
 {
@@ -7,6 +10,45 @@ namespace WebAPI.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        public BooksController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> GetBooks()
+        {
+            var result = await _mediator.Send(new GetBookListQuery());
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById([FromRoute] int id)
+        {
+            var result = await _mediator.Send(new GetBookByIdQuery(id));
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateBook([FromBody] UpdateBookCommand command)
+        {
+            await _mediator.Send(command);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> RemoveBook(int id)
+        {
+            await _mediator.Send(new RemoveBookCommand(id));
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBook([FromBody] CreateBookCommand command)
+        {
+            await _mediator.Send(command);
+            return Created("", command);
+        }
     }
 }
